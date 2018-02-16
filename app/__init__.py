7b0257cf69c557de2ea76093384cffedb83e5d01
brittany marquis <brittany.marquis@mymona.uwi.edu>
2018-02-16 20:41:51 +0000

    inital commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='enter some random passphase'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '2525'
+app.config['MAIL_USERNAME'] = '908f11bafc78eb'
+app.config['MAIL_PASSWORD'] = 'fc923bb07a2f90'
+
+mail = Mail(app)
 from app import views
